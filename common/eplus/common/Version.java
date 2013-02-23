9d7718906c1b8c6b906ee2d7c0758d93d1ac10e8
odininon <Mike.stengel@gmail.com>
2013-02-23 09:32:35 -0500

    updated to version 1.12.9

--- common/eplus/common/Version.java
+++ common/eplus/common/Version.java
@@ -14,8 +14,7 @@ import java.util.logging.Level;
 
 public class Version {
 
-    public static final String VERSION = "@VERSION@";
-    public static final String BUILD_NUMBER = "@BUILD_NUMBER@";
+    public static final String VERSION = "1.12.9";
     private static final String REMOTE_VERSION_FILE = "https://raw.github.com/odininon/EnchantingPlus/master/resources/version";//"https://raw.github.com/odininon/EnchantingPlus/1.4.6/resources/version";
     public static EnumUpdateState currentVersion = EnumUpdateState.CURRENT;
 
