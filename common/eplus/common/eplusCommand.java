ebe4848dcce25d0d4379c6d3e20b3c4de30c8dee
odininon <Mike.stengel@gmail.com>
2013-02-23 14:18:01 -0500

    -Changelog command, usage: /eplus changelog

--- common/eplus/common/eplusCommand.java
+++ common/eplus/common/eplusCommand.java
@@ -28,22 +28,31 @@ public class eplusCommand extends CommandBase {
 
     @Override
     public void processCommand(ICommandSender var1, String[] args) {
-        if (args.length > 1) {
+        if (args.length > 0) {
             String commandName = args[0];
             System.arraycopy(args, 1, args, 0, args.length - 1);
 
             if (commandName.equalsIgnoreCase("texture")) {
                 processTexture(var1, args);
+            } else if (commandName.equalsIgnoreCase("changelog")){
+                processChangelog(var1, args);
             } else {
-                throw new WrongUsageException("eplus [ texture ]", new Object[]{0});
+                throw new WrongUsageException("eplus [ texture | changelog ]", new Object[]{0});
             }
         } else {
-            throw new WrongUsageException("eplus [ texture ]", new Object[]{0});
+            throw new WrongUsageException("eplus [ texture | changelog ]", new Object[]{0});
+        }
+    }
+
+    private void processChangelog(ICommandSender var1, String[] args) {
+        var1.sendChatToPlayer("[EPLUS] Changelog:");
+        for(String line : Version.grabChangelog()){
+            var1.sendChatToPlayer("-" + line);
         }
     }
 
     private void processTexture(ICommandSender var1, String[] args) {
-        if (args.length > 1) {
+        if (args.length > 0) {
             int value = Integer.valueOf(args[0]);
 
             if (value < 0) {
@@ -69,7 +78,7 @@ public class eplusCommand extends CommandBase {
 
         switch (args.length) {
             case 1:
-                return getListOfStringsMatchingLastWord(args, new String[]{"texture"});
+                return getListOfStringsMatchingLastWord(args, new String[]{"texture", "changelog"});
             case 2:
                 if (args[0].equalsIgnoreCase("texture")) {
                     return getListOfStringsMatchingLastWord(args, new String[]{"0", "1", "2"});
