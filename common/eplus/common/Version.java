325a9cb0efe53cafd8272dfd4aeec00dd11526fd
odininon <Mike.stengel@gmail.com>
2013-02-26 15:16:50 -0500

    inlcude build number into changelog in game.

--- common/eplus/common/Version.java
+++ common/eplus/common/Version.java
@@ -18,6 +18,7 @@ public class Version implements Runnable {
     public static Version instance = new Version();
 
     public static final String VERSION = "1.12.9";
+    public static final String BUILD = "@BUILD_NUMBER@";
     private static final String REMOTE_VERSION_FILE = "https://raw.github.com/odininon/EnchantingPlus/master/resources/version";//"https://raw.github.com/odininon/EnchantingPlus/1.4.6/resources/version";
     public static EnumUpdateState currentVersion = EnumUpdateState.CURRENT;
 
@@ -34,7 +35,7 @@ public class Version implements Runnable {
 
     public static String getCurrentModVersion()
     {
-        return currentModVersion;
+        return (currentVersion == EnumUpdateState.BETA) ? currentModVersion + "." + BUILD : currentModVersion;
     }
 
     public static boolean versionSeen() {
