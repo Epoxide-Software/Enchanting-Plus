25dbb0560ad13c58aa6d2f0ac3cb6241105823c4
Freyja <mike.stengel@gmail.com>
2013-06-15 02:48:06 -0400

    Added Treecapitator enchantment tool-tip

---
+++ src/eplus/plugins/Treecapitatior.java
@@ -0,0 +1,17 @@
+package eplus.plugins;
+
+import eplus.api.EplusApi;
+
+/**
+ * @author Freyja
+ *         Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+@EplusPlugin
+public class Treecapitatior {
+
+    @EplusPlugin.PreInit
+    public void preInit()
+    {
+        EplusApi.addCustomEnchantmentToolTip("Treecapitating", "Allows axe's not in the config to chop down trees Treecapitator style.");
+    }
+}
