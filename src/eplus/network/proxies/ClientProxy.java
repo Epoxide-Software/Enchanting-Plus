e702899a226aafcee3fe88de8ee8296b11268664
Freyja <mike.stengel@gmail.com>
2013-07-29 18:48:39 -0400

    -Added textures provided by ShadowClaimer

--- src/eplus/network/proxies/ClientProxy.java
+++ src/eplus/network/proxies/ClientProxy.java
@@ -1,11 +1,18 @@
 package eplus.network.proxies;
 
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.ForgeHooks;
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 import eplus.exceptions.FingerprintException;
 import eplus.handlers.CapeTickHandler;
 import eplus.handlers.VersionTickHandler;
+import eplus.inventory.TileEnchantTable;
 import eplus.lib.EnchantmentHelp;
+import eplus.renders.EnchantmentTableRender;
 
 /**
  * @user odininon
@@ -18,7 +25,8 @@ public class ClientProxy extends CommonProxy
     {
         TickRegistry.registerTickHandler(new CapeTickHandler(), Side.CLIENT);
         TickRegistry.registerScheduledTickHandler(new VersionTickHandler(), Side.CLIENT);
-
+        
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEnchantTable.class, new EnchantmentTableRender());
         EnchantmentHelp.init();
     }
 
