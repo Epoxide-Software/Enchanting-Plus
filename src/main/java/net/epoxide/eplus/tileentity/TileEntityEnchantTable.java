b61785c08d66cbe46450774d39566768a3c2fc35
lclc98 <liam.c14785@gmail.com>
2015-10-29 20:18:10 +1000

    Removed unused methods

--- src/main/java/net/epoxide/eplus/tileentity/TileEntityEnchantTable.java
+++ src/main/java/net/epoxide/eplus/tileentity/TileEntityEnchantTable.java
@@ -10,13 +10,13 @@ public class TileEntityEnchantTable extends TileEntity {
     public int tickCount;
     public float pageFlip;
     public float pageFlipPrev;
-    public float field_145932_k;
+    public float pageFlipRandom;
     public float pageFlipTurn;
     public float field_145930_m;
     public float field_145927_n;
     public float field_145928_o;
     public float field_145925_p;
-    public float field_145924_q;
+    public float bookRotation;
     private static Random random = new Random();
     private String customName;
 
@@ -25,7 +25,7 @@ public class TileEntityEnchantTable extends TileEntity {
 
         super.writeToNBT(compound);
 
-        if (this.func_145921_b()) {
+        if (this.hasCustomInventoryName()) {
             compound.setString("CustomName", this.customName);
         }
     }
@@ -51,19 +51,19 @@ public class TileEntityEnchantTable extends TileEntity {
         if (entityplayer != null) {
             double d0 = entityplayer.posX - (double) ((float) this.xCoord + 0.5F);
             double d1 = entityplayer.posZ - (double) ((float) this.zCoord + 0.5F);
-            this.field_145924_q = (float) Math.atan2(d1, d0);
+            this.bookRotation = (float) Math.atan2(d1, d0);
             this.field_145930_m += 0.1F;
 
             if (this.field_145930_m < 0.5F || random.nextInt(40) == 0) {
-                float f1 = this.field_145932_k;
+                float f1 = this.pageFlipRandom;
 
                 do {
-                    this.field_145932_k += (float) (random.nextInt(4) - random.nextInt(4));
-                } while (f1 == this.field_145932_k);
+                    this.pageFlipRandom += (float) (random.nextInt(4) - random.nextInt(4));
+                } while (f1 == this.pageFlipRandom);
             }
         }
         else {
-            this.field_145924_q += 0.02F;
+            this.bookRotation += 0.02F;
             this.field_145930_m -= 0.1F;
         }
 
@@ -75,17 +75,17 @@ public class TileEntityEnchantTable extends TileEntity {
             this.field_145928_o += ((float) Math.PI * 2F);
         }
 
-        while (this.field_145924_q >= (float) Math.PI) {
-            this.field_145924_q -= ((float) Math.PI * 2F);
+        while (this.bookRotation >= (float) Math.PI) {
+            this.bookRotation -= ((float) Math.PI * 2F);
         }
 
-        while (this.field_145924_q < -(float) Math.PI) {
-            this.field_145924_q += ((float) Math.PI * 2F);
+        while (this.bookRotation < -(float) Math.PI) {
+            this.bookRotation += ((float) Math.PI * 2F);
         }
 
         float f2;
 
-        f2 = this.field_145924_q - this.field_145928_o;
+        f2 = this.bookRotation - this.field_145928_o;
         while (f2 >= (float) Math.PI) {
             f2 -= ((float) Math.PI * 2F);
         }
@@ -106,7 +106,7 @@ public class TileEntityEnchantTable extends TileEntity {
 
         ++this.tickCount;
         this.pageFlipPrev = this.pageFlip;
-        float f = (this.field_145932_k - this.pageFlip) * 0.4F;
+        float f = (this.pageFlipRandom - this.pageFlip) * 0.4F;
         float f3 = 0.2F;
 
         if (f < -f3) {
@@ -121,18 +121,8 @@ public class TileEntityEnchantTable extends TileEntity {
         this.pageFlip += this.pageFlipTurn;
     }
 
-    public String func_145919_a () {
-
-        return this.func_145921_b() ? this.customName : "container.enchant";
-    }
-
-    public boolean func_145921_b () {
+    public boolean hasCustomInventoryName () {
 
         return this.customName != null && this.customName.length() > 0;
-    }
-
-    public void func_145920_a (String customName) {
-
-        this.customName = customName;
     }
 }
