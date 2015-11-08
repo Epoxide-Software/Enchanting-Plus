08b485ed53cbeafd44711020a8e4644b33a758b1
Darkhax <darklime@live.ca>
2015-11-07 23:34:59 -0700

    Added modifier tooltips.

--- src/main/java/net/epoxide/eplus/handler/ForgeEventHandler.java
+++ src/main/java/net/epoxide/eplus/handler/ForgeEventHandler.java
@@ -1,13 +1,45 @@
 package net.epoxide.eplus.handler;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.darkhax.bookshelf.lib.util.ItemStackUtils;
 import net.epoxide.eplus.common.PlayerProperties;
+import net.epoxide.eplus.modifiers.ScrollModifier;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
 public class ForgeEventHandler {
+    
+    @SubscribeEvent
+    public void onTooltip (ItemTooltipEvent event) {
+        
+        if (ItemStackUtils.isValidStack(event.itemStack)) {
+            
+            for (ScrollModifier modifier : ContentHandler.modifiers) {
+                
+                if (ItemStackUtils.areStacksSimilar(event.itemStack, modifier.stack)) {
+                    
+                    event.toolTip.add(EnumChatFormatting.LIGHT_PURPLE + StatCollector.translateToLocal("tooltip.eplus.modifier"));
+                    
+                    if (modifier.speed != 0.00d) {
+                        
+                        boolean isPositive = modifier.speed > 0.00d;
+                        event.toolTip.add(((isPositive) ? EnumChatFormatting.GREEN : EnumChatFormatting.RED) + StatCollector.translateToLocal("tooltip.eplus.speed") + ": " + ((isPositive) ? "+" : "") + modifier.speed * 100 + "%");
+                    }
+                    
+                    if (modifier.stability != 0.00d) {
+                        
+                        boolean isPositive = modifier.stability > 0.00d;
+                        event.toolTip.add(((isPositive) ? EnumChatFormatting.GREEN : EnumChatFormatting.RED) + StatCollector.translateToLocal("tooltip.eplus.stability") + ": " + ((isPositive) ? "+" : "") + modifier.stability * 100 + "%");
+                    }
+                }
+            }
+        }
+    }
     
     @SubscribeEvent
     public void onPlayerClone (PlayerEvent.Clone event) {
