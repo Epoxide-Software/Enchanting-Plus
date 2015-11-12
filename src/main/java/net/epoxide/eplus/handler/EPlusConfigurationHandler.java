0cee827362a6267bf864d1509598761fe0b78a07
Darkhax <darklime@live.ca>
2015-11-12 00:43:12 -0700

    Added some new config options.

--- src/main/java/net/epoxide/eplus/handler/EPlusConfigurationHandler.java
+++ src/main/java/net/epoxide/eplus/handler/EPlusConfigurationHandler.java
@@ -15,9 +15,10 @@ public class EPlusConfigurationHandler {
     public static boolean allowDamagedEnchanting = true;
     public static boolean allowModifierTooltips = true;
     public static boolean needsBookShelves = true;
+    public static boolean secureItems = true;
+    public static boolean allowUnownedModifications = true;
     
     // TODO implement these
-    public static boolean allowDisenUnowned = true;
     public static int costFactor = 5;
     public static int repairFactor = 5;
     public static int minimumBookshelfs = 5;
@@ -34,6 +35,8 @@ public class EPlusConfigurationHandler {
         allowDamagedEnchanting = config.getBoolean("allowDamagedEnchanting", settings, allowDamagedEnchanting, "If this is true, players will be able to enchant items which are not at full durability.");
         allowModifierTooltips = config.getBoolean("allowModifierTooltips", settings, allowModifierTooltips, "If true, special information will be written to the Scroll Modifier item tooltips.");
         minimumBookshelfs = config.getInt("minBookshelfs", settings, minimumBookshelfs, 0, 24, "The lowest number of bookshelfs required for the table to function properly.");
+        secureItems = config.getBoolean("secureItems", settings, secureItems, "If enabled, players will only be able to enchant enchanted items if they inchanted them first.");
+        allowUnownedModifications = config.getBoolean("allowUnownedModifications", settings, allowUnownedModifications, "If enabled, players will be able to modify items which are not currently owned by any other player. IE: Dungeon Loot");
         
         if (config.hasChanged())
             config.save();
