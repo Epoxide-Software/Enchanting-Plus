62189c389afba57011e9c9605d8bb8d0f60e1347
Darkhax <darklime@live.ca>
2015-11-12 22:23:26 -0700

    Format + Imports

--- src/main/java/net/epoxide/eplus/common/network/PacketEnchant.java
+++ src/main/java/net/epoxide/eplus/common/network/PacketEnchant.java
@@ -1,11 +1,11 @@
 package net.epoxide.eplus.common.network;
 
+import java.util.HashMap;
+
 import io.netty.buffer.ByteBuf;
 import net.darkhax.bookshelf.common.network.AbstractMessage;
 import net.epoxide.eplus.inventory.ContainerEnchantTable;
 import net.minecraft.entity.player.EntityPlayer;
-
-import java.util.HashMap;
 
 /**
  * This class was created on 10/31/2015 by lclc98.
@@ -13,31 +13,32 @@ import java.util.HashMap;
 public class PacketEnchant extends AbstractMessage {
     protected int totalCost;
     protected HashMap<Integer, Integer> enchants = new HashMap<Integer, Integer>();
-
-    public PacketEnchant () {
-
+    
+    public PacketEnchant() {
+    
     }
-
-    public PacketEnchant (HashMap<Integer, Integer> enchants, int totalCost) {
-
+    
+    public PacketEnchant(HashMap<Integer, Integer> enchants, int totalCost) {
+        
         this.enchants = enchants;
         this.totalCost = totalCost;
     }
-
+    
     @Override
     public void handleClientMessage (AbstractMessage message, EntityPlayer player) {
-
+    
     }
-
+    
     @Override
     public void handleServerMessage (AbstractMessage message, EntityPlayer player) {
-
+        
         if (message instanceof PacketEnchant) {
             PacketEnchant packet = (PacketEnchant) message;
             if (player.openContainer instanceof ContainerEnchantTable) {
                 try {
                     ((ContainerEnchantTable) player.openContainer).enchant(player, packet.enchants, packet.totalCost);
-                } catch (final Exception e) {
+                }
+                catch (final Exception e) {
                     // EnchantingPlus.log.info("Enchanting failed because: " +
                     // e.getLocalizedMessage());
                 }
@@ -45,29 +46,29 @@ public class PacketEnchant extends AbstractMessage {
             }
         }
     }
-
+    
     @Override
     public void fromBytes (ByteBuf buf) {
-
+        
         final HashMap<Integer, Integer> enchants = new HashMap<Integer, Integer>();
-
+        
         this.totalCost = buf.readInt();
-
+        
         int size = buf.readInt();
-
+        
         for (int i = 0; i < size; i++) {
             enchants.put(buf.readInt(), buf.readInt());
         }
-
+        
         this.enchants = enchants;
     }
-
+    
     @Override
     public void toBytes (ByteBuf buf) {
-
+        
         buf.writeInt(totalCost);
         buf.writeInt(enchants.size());
-
+        
         for (Integer enchantmentId : enchants.keySet()) {
             buf.writeInt(enchantmentId);
             buf.writeInt(enchants.get(enchantmentId));
--- src/main/java/net/epoxide/eplus/inventory/EnchantHelper.java
+++ src/main/java/net/epoxide/eplus/inventory/EnchantHelper.java
@@ -1,5 +1,7 @@
 package net.epoxide.eplus.inventory;
 
+import java.util.List;
+
 import net.darkhax.bookshelf.handler.BookshelfHooks;
 import net.epoxide.eplus.common.PlayerProperties;
 import net.epoxide.eplus.handler.EPlusConfigurationHandler;
@@ -12,55 +14,53 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 
-import java.util.List;
-
 public class EnchantHelper {
-
+    
     public static boolean isEnchantmentValid (Enchantment ench, EntityPlayer entityPlayer) {
-
+        
         return ench != null && ((EPlusConfigurationHandler.useQuestMode && PlayerProperties.getProperties(entityPlayer).unlockedEnchantments.contains(ench.effectId)) || entityPlayer.capabilities.isCreativeMode);
     }
-
+    
     public static boolean isEnchantmentsCompatible (Enchantment ench1, Enchantment ench2) {
-
+        
         return ench1.canApplyTogether(ench2) && ench2.canApplyTogether(ench1);
     }
-
+    
     public static boolean isItemEnchanted (ItemStack itemStack) {
-
+        
         return itemStack.hasTagCompound() && (itemStack.getItem() != Items.enchanted_book ? itemStack.stackTagCompound.hasKey("ench") : itemStack.stackTagCompound.hasKey("StoredEnchantments"));
     }
-
+    
     public static boolean isNewItemEnchantable (Item item) {
-
+        
         if (item.equals(Items.enchanted_book)) {
             return isItemEnchantable(new ItemStack(Items.book));
         }
         return isItemEnchantable(new ItemStack(item));
     }
-
+    
     public static boolean isItemEnchantable (ItemStack itemStack) {
-
+        
         boolean flag = !itemStack.hasTagCompound() || !itemStack.getTagCompound().hasKey("charge");
-
+        
         return itemStack.getItem().getItemEnchantability(itemStack) > 0 && (itemStack.getItem() == Items.book || itemStack.getItem() == Items.enchanted_book || itemStack.isItemEnchantable() && flag);
     }
-
+    
     /**
      * @param enchantmentData : The Enchantment Data that is set onto the itemstack
-     * @param itemStack       : The itemstack currently in the enchantment table
-     * @param player          :    The current player that hit the button to enchant the item the
-     *                        enchantment table
+     * @param itemStack : The itemstack currently in the enchantment table
+     * @param player : The current player that hit the button to enchant the item the
+     *            enchantment table
      * @param cost
      * @return
      */
     public static ItemStack setEnchantments (List<EnchantmentData> enchantmentData, ItemStack itemStack, EntityPlayer player, int cost) {
-
+        
         if (hasRestriction(itemStack) && !restrictionMatches(itemStack, player))
             return itemStack;
-
+            
         enchantmentData = BookshelfHooks.onItemEnchanted(player, itemStack, cost, enchantmentData);
-
+        
         NBTTagList nbttaglist = new NBTTagList();
         for (final EnchantmentData data : enchantmentData) {
             final NBTTagCompound nbttagcompound = new NBTTagCompound();
@@ -68,12 +68,12 @@ public class EnchantHelper {
             nbttagcompound.setInteger("lvl", data.enchantmentLevel);
             nbttaglist.appendTag(nbttagcompound);
         }
-
+        
         if (itemStack.getItem() == Items.book)
             itemStack = new ItemStack(Items.enchanted_book);
-
+            
         if (itemStack.getItem() == Items.enchanted_book) {
-
+            
             if (nbttaglist.tagCount() > 0) {
                 itemStack.setTagInfo("StoredEnchantments", nbttaglist);
             }
@@ -89,22 +89,22 @@ public class EnchantHelper {
         else if (itemStack.hasTagCompound()) {
             itemStack.getTagCompound().removeTag("ench");
             itemStack.getTagCompound().removeTag("enchantedOwnerUUID");
-
+            
         }
         return itemStack;
     }
-
+    
     public static boolean hasRestriction (ItemStack itemStack) {
-
+        
         if (itemStack.hasTagCompound()) {
             String enchantedOwner = itemStack.getTagCompound().getString("enchantedOwnerUUID");
             return !enchantedOwner.equals("");
         }
         return false;
     }
-
+    
     public static boolean restrictionMatches (ItemStack itemStack, EntityPlayer player) {
-
+        
         String enchantedOwner = itemStack.getTagCompound().getString("enchantedOwnerUUID");
         return player.getUniqueID().toString().equals(enchantedOwner);
     }
