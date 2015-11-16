b8971399de72f7cbcc33649623c64a782c8643e4
Darkhax <darklime@live.ca>
2015-11-16 14:56:32 -0700

    Hooked up the rest of the config file properties.

--- src/main/java/net/epoxide/eplus/handler/EPlusConfigurationHandler.java
+++ src/main/java/net/epoxide/eplus/handler/EPlusConfigurationHandler.java
@@ -20,8 +20,6 @@ public class EPlusConfigurationHandler {
     public static boolean allowVillagers = true;
     public static int villagerID = 935153;
     public static int maxEnchantmentAmount = 5;
-    
-    // TODO implement these
     public static int costFactor = 5;
     public static int repairFactor = 5;
     public static int minimumBookshelfs = 5;
@@ -42,7 +40,9 @@ public class EPlusConfigurationHandler {
         allowUnownedModifications = config.getBoolean("allowUnownedModifications", settings, allowUnownedModifications, "If enabled, players will be able to modify items which are not currently owned by any other player. IE: Dungeon Loot");
         allowVillagers = config.getBoolean("allowVillagers", settings, allowVillagers, "If enabled, a custom E+ villager will spawn and generate in villages.");
         villagerID = config.getInt("villagerID", settings, villagerID, Integer.MIN_VALUE, Integer.MAX_VALUE, "A unique ID for the E+ villager. This should almost never need to be changed.");
-        
+        costFactor = config.getInt("costFactor", settings, costFactor, 0, 128, "A number used when calculated enchantment costs. A higher factor means higher enchantment costs.");
+        repairFactor = config.getInt("repairFactor", settings, repairFactor, 0, 128, "A number used when calculating the repair costs. A higher factor means higher repair costs.");
+        minimumBookshelfs = config.getInt("minimumBookshelfs", settings, minimumBookshelfs, 0, 64, "The minimum amount of Bookshelfs required to use the enchantment table.");
         if (config.hasChanged())
             config.save();
     }
