2b9b7f464bec3373e9cdd7df2ce51e392ecbacba
lclc98 <liam.c14785@gmail.com>
2016-01-05 12:30:29 +1000

    Fixed bug with rendering of effect

--- src/main/java/net/epoxide/eplus/buff/BuffBookFall.java
+++ src/main/java/net/epoxide/eplus/buff/BuffBookFall.java
@@ -24,26 +24,27 @@ import net.epoxide.eplus.handler.ContentHandler;
 import net.epoxide.eplus.lib.util.RenderUtil;
 
 public class BuffBookFall extends Buff {
-    
+
     private static ResourceLocation texture = new ResourceLocation("eplus:textures/entity/enchantingplus_book.png");
     private static ResourceLocation icon = new ResourceLocation("eplus:textures/buffs/enchanted_guardian.png");
-    
+
     public BuffBookFall() {
-        
+
         super("eplus.book", false, new Color(128, 0, 128).getRGB(), icon);
         MinecraftForge.EVENT_BUS.register(this);
     }
-    
+
     @SideOnly(Side.CLIENT)
     @SubscribeEvent
     public void onPlayerRender (RenderLivingEvent.Specials.Pre event) {
-        
+
         if (shouldUseEffect(event.entity)) {
-            
+
             GL11.glPushMatrix();
             GL11.glEnable(GL11.GL_BLEND);
             GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            GL11.glTranslatef(0f, -2.1f, 0f);
+            GL11.glTranslated(event.x, event.y, event.z);
+            GL11.glTranslatef(0f, -0.5f, 0f);
             GL11.glRotatef(26f, 0f, -1f, 0f);
             GL11.glRotatef(event.entity.rotationYaw, 0, -1f, 0);
             GL11.glScalef(4f, 4, 4f);
@@ -51,27 +52,23 @@ public class BuffBookFall extends Buff {
             GL11.glPopMatrix();
         }
     }
-    
+
     @SubscribeEvent
     public void onPlayerUpdate (LivingUpdateEvent event) {
-        
+
         if (shouldUseEffect(event.entityLiving)) {
-            
+
             event.entityLiving.motionY *= 0.45D;
             event.entityLiving.fallDistance = 0;
         }
     }
-    
+
     public boolean shouldUseEffect (EntityLivingBase entity) {
-        
+
         if (BuffHelper.hasBuff(entity, ContentHandler.buffFloatingBook) && !entity.onGround && !entity.isInWater() && !entity.isInsideOfMaterial(Material.water)) {
-            
-            if (entity instanceof EntityPlayer && ((EntityPlayer) entity).capabilities.isFlying)
-                return true;
-                
             return (entity.motionY < 0.0f);
         }
-        
+
         return false;
     }
 }
