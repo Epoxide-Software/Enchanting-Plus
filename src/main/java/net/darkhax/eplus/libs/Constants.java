e2781ee4185091bc7c3744e83b099525634a6d02
Tyler Hancock <darklime@live.ca>
2016-04-22 03:06:17 -0600

    Updated min forge version.

--- src/main/java/net/darkhax/eplus/libs/Constants.java
+++ src/main/java/net/darkhax/eplus/libs/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     public static final String MOD_NAME = "Enchanting Plus";
     public static final String VERSION_NUMBER = "4.1.0.0";
     public static final Logger LOG = LogManager.getLogger(MOD_NAME);
-    public static final String DEPENDENCIES = "required-after:bookshelf@[1.0.4.172,)";
+    public static final String DEPENDENCIES = "required-after:bookshelf@[1.2.0.219,)";
     
     public static final String FACTORY = "";
     public static final String CLIENT_PROXY_CLASS = "net.darkhax.eplus.client.ProxyClient";
