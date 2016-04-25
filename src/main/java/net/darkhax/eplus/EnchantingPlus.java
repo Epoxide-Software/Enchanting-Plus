1fd33501b62a90e72e9d4d3b96f17017f76d2416
Tyler Hancock <darklime@live.ca>
2016-04-24 18:11:03 -0600

    Added repair packet.

--- src/main/java/net/darkhax/eplus/EnchantingPlus.java
+++ src/main/java/net/darkhax/eplus/EnchantingPlus.java
@@ -2,6 +2,7 @@ package net.darkhax.eplus;
 
 import net.darkhax.eplus.common.ProxyCommon;
 import net.darkhax.eplus.common.network.GuiHandler;
+import net.darkhax.eplus.common.network.packet.PacketRepairItem;
 import net.darkhax.eplus.creativetab.CreativeTabEPlus;
 import net.darkhax.eplus.handler.ConfigurationHandler;
 import net.darkhax.eplus.handler.ContentHandler;
@@ -19,6 +20,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, guiFactory = Constants.FACTORY, dependencies = Constants.DEPENDENCIES)
 public final class EnchantingPlus {
@@ -62,6 +64,7 @@ public final class EnchantingPlus {
     public void preInit (FMLPreInitializationEvent event) {
         
         network = NetworkRegistry.INSTANCE.newSimpleChannel("EnchantingPlus");
+        network.registerMessage(PacketRepairItem.PacketHandler.class, PacketRepairItem.class, 2, Side.SERVER);
         
         NetworkRegistry.INSTANCE.registerGuiHandler(instance, new GuiHandler());
         
