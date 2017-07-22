62ddd4a494dea149f1c5834f2d775ad8bd4ae960
Tyler Hancock <darklime@live.ca>
2017-07-21 18:52:51 -0600

    Started class for reusable E+ code. //TODO merge into bookshelf later.

---
+++ src/main/java/net/darkhax/eplus/libs/EPlusUtils.java
@@ -0,0 +1,50 @@
+package net.darkhax.eplus.libs;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
+public class EPlusUtils {
+    
+    public static String getEnchId(Enchantment enchant) {
+        
+        if (enchant == null) {
+            
+            Constants.LOG.warn("Attempted to get ID for invalid enchantment. Enchantment was null.");
+            return null;
+        }
+        
+        else if (enchant.getRegistryName() == null || enchant.getRegistryName().toString().isEmpty()) {
+            
+            Constants.LOG.warn("Attempted to get ID for invalid enchantment. Enchantment lacks registry info. Class: %s Name: %s", enchant.getClass(), enchant.getName());
+            return null;
+        }
+        
+        else if (!ForgeRegistries.ENCHANTMENTS.containsValue(enchant) || !ForgeRegistries.ENCHANTMENTS.containsKey(enchant.getRegistryName())) {
+            
+            Constants.LOG.warn("Attempted to get ID for invalid enchantment. Enchantment was not registered. Class: %s Name: %s RegID: %S", enchant.getClass(), enchant.getName(), enchant.getRegistryName().toString());
+            return null;
+        }
+        
+        return enchant.getRegistryName().toString();
+    }
+    
+    public static Enchantment getEnchantmentFromId(String id) {
+        
+        if (id == null || id.isEmpty()) {
+            
+            Constants.LOG.warn("Attempted to get Enchantment for invalid ID. ID was empty or null.");
+            return null;
+        }
+        
+        final ResourceLocation idRL = new ResourceLocation(id);
+        
+        if (!ForgeRegistries.ENCHANTMENTS.containsKey(idRL)) {
+            
+            Constants.LOG.warn("Attempted to get Enchantment for invalid ID. No enchantment registered with id %s", id);
+            return null;
+        }
+        
+        return ForgeRegistries.ENCHANTMENTS.getValue(idRL);
+    }
+}
