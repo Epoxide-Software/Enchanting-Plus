fb1cafd6be54911be63b04069c2a6c2f3fb70d22
Tyler Hancock <darklime@live.ca>
2017-07-25 13:30:04 -0600

    Updated various warning messages.

--- src/main/java/net/darkhax/eplus/libs/EPlusUtils.java
+++ src/main/java/net/darkhax/eplus/libs/EPlusUtils.java
@@ -1,50 +1,58 @@
 package net.darkhax.eplus.libs;
 
+import com.google.common.base.Throwables;
+
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class EPlusUtils {
-    
-    public static String getEnchId(Enchantment enchant) {
-        
+
+    public static String getEnchId (Enchantment enchant) {
+
         if (enchant == null) {
-            
-            Constants.LOG.warn("Attempted to get ID for invalid enchantment. Enchantment was null.");
+
+            Constants.LOG.warn(String.format("Attempted to get ID for invalid enchantment. Enchantment was null."), Throwables.getStackTraceAsString(new Throwable()));
             return "N/A";
         }
-        
+
         else if (enchant.getRegistryName() == null || enchant.getRegistryName().toString().isEmpty()) {
-            
-            Constants.LOG.warn("Attempted to get ID for invalid enchantment. Enchantment lacks registry info. Class: %s Name: %s", enchant.getClass(), enchant.getName());
+
+            Constants.LOG.warn(String.format("Attempted to get ID for invalid enchantment. Enchantment lacks registry info. Class: %s Name: %s", enchant.getClass(), enchant.getName()), Throwables.getStackTraceAsString(new Throwable()));
             return "N/A";
         }
-        
+
         else if (!ForgeRegistries.ENCHANTMENTS.containsValue(enchant) || !ForgeRegistries.ENCHANTMENTS.containsKey(enchant.getRegistryName())) {
-            
-            Constants.LOG.warn("Attempted to get ID for invalid enchantment. Enchantment was not registered. Class: %s Name: %s RegID: %S", enchant.getClass(), enchant.getName(), enchant.getRegistryName().toString());
+
+            Constants.LOG.warn(String.format("Attempted to get ID for invalid enchantment. Enchantment was not registered. Class: %s Name: %s RegID: %S", enchant.getClass(), enchant.getName(), enchant.getRegistryName().toString()), Throwables.getStackTraceAsString(new Throwable()));
             return "N/A";
         }
-        
+
         return enchant.getRegistryName().toString();
     }
-    
-    public static Enchantment getEnchantmentFromId(String id) {
-        
-        if (id == null || id.isEmpty()) {
-            
-            Constants.LOG.warn("Attempted to get Enchantment for invalid ID. ID was empty or null.");
+
+    public static Enchantment getEnchantmentFromId (String id) {
+
+        if (id == null) {
+
+            Constants.LOG.warn(String.format("Attempted to get Enchantment for invalid ID. ID was null."), Throwables.getStackTraceAsString(new Throwable()));
             return null;
         }
-        
+
+        if (id.isEmpty()) {
+
+            Constants.LOG.warn(String.format("Attempted to get Enchantment for invalid ID. ID was empty."), Throwables.getStackTraceAsString(new Throwable()));
+            return null;
+        }
+
         final ResourceLocation idRL = new ResourceLocation(id);
-        
+
         if (!ForgeRegistries.ENCHANTMENTS.containsKey(idRL)) {
-            
-            Constants.LOG.warn("Attempted to get Enchantment for invalid ID. No enchantment registered with id %s", id);
+
+            Constants.LOG.warn(String.format("Attempted to get Enchantment for invalid ID. No enchantment registered with id %s", id), Throwables.getStackTraceAsString(new Throwable()));
             return null;
         }
-        
+
         return ForgeRegistries.ENCHANTMENTS.getValue(idRL);
     }
 }
