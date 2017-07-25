df47f63c46b5017bc1d3d114562b6f48842018ac
Tyler Hancock <darklime@live.ca>
2017-07-25 13:46:13 -0600

    Implemented new logger code.

--- src/main/java/net/darkhax/eplus/libs/Constants.java
+++ src/main/java/net/darkhax/eplus/libs/Constants.java
@@ -1,14 +1,13 @@
 package net.darkhax.eplus.libs;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.darkhax.bookshelf.lib.LoggingHelper;
 
 public final class Constants {
 
     public static final String MOD_ID = "eplus";
     public static final String MOD_NAME = "Enchanting Plus";
     public static final String VERSION_NUMBER = "4.1.0.0";
-    public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+    public static final LoggingHelper LOG = new LoggingHelper(MOD_NAME);
     public static final String DEPENDENCIES = "required-after:bookshelf@[1.4.1.320,)";
 
     public static final String FACTORY = "";
