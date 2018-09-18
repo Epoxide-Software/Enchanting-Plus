86179f9367f77bb855b7a1f41d61b74bedefb1a9
Tyler Hancock <darklime@live.ca>
2018-09-17 18:26:41 -0600

    Added CraftTweaker support.

---
+++ src/main/java/net/darkhax/eplus/compat/crt/EnchantingPlusCRT.java
@@ -0,0 +1,72 @@
+package net.darkhax.eplus.compat.crt;
+
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.enchantments.IEnchantment;
+import crafttweaker.api.item.IItemStack;
+import net.darkhax.eplus.api.Blacklist;
+import net.minecraft.item.ItemStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+@ZenRegister
+@ZenClass("mods.eplus.Eplus")
+public class EnchantingPlusCRT {
+    
+    @ZenMethod
+    public static void blacklistEnchantment(IItemStack stack) {
+        
+        CraftTweakerAPI.apply(new ActionBlacklistItem(stack));
+    }
+    
+    @ZenMethod
+    public static void blacklistItem(IEnchantment enchantment) {
+        
+        CraftTweakerAPI.apply(new ActionBlacklist(enchantment));
+    }
+    
+    private static class ActionBlacklistItem implements IAction {
+
+        private IItemStack item;
+        
+        public ActionBlacklistItem(IItemStack stack) {
+            
+            this.item = stack;
+        }
+        
+        @Override
+        public void apply () {
+            
+            Blacklist.blacklist((ItemStack) this.item.getDefinition().getInternal());
+        }
+
+        @Override
+        public String describe () {
+            
+            return "Blacklisting " + this.item.getDisplayName() + " from E+";
+        }
+    }
+    
+    private static class ActionBlacklist implements IAction {
+
+        private IEnchantment enchantment;
+        
+        public ActionBlacklist(IEnchantment enchantment) {
+            
+            this.enchantment = enchantment;
+        }
+        
+        @Override
+        public void apply () {
+            
+            Blacklist.blacklist((ItemStack) this.enchantment.getDefinition().getInternal());
+        }
+
+        @Override
+        public String describe () {
+            
+            return "Blacklisting " + this.enchantment.displayName() + " from E+";
+        }
+    }
+}
